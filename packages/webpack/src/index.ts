f22b565d12025d249864bc34df3346e644a17ce3
Arshia Moghaddam <ars2062@gmail.com>
2022-12-29 16:42:24 +0330

    chore: webpack plugin initial commit

---
+++ packages/webpack/src/index.ts
@@ -0,0 +1,22 @@
+import { Compiler } from "webpack";
+import { ImageExtensions, IOptions } from "react-image-to-dns-core";
+
+export default class ImageToDNSPlugin {
+  options: IOptions;
+  constructor(options: IOptions = {}) {
+    Object.assign(options, {
+      extensions: ImageExtensions,
+      cdnPath: "/cdn",
+      breakpoints: [320, 375, 425, 768, 1024, 1440],
+      sizeOptimization: 80,
+    });
+    this.options = options;
+  }
+  apply(compiler: Compiler) {
+    console.log(compiler);
+    compiler.hooks.done.tapAsync("ImageToDNSPlugin", (stats, cb) => {
+      console.log(stats);
+      cb();
+    });
+  }
+}
